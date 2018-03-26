b0401cc415e0c737d5846ef40542692b365583da
jzz2649 <1563382649@qq.com>
2018-03-26 13:52:36 +0800

    Update heapSort.js

--- heapSort.js
+++ heapSort.js
@@ -1,50 +1,21 @@
-function Tree(list){
-  this.tree = [];
-  this.layer = -1;
-  this.max = 0;
-  for(var i = 0; i < list.length; i++) {
-    this.switch(this.push(list[i]), this.layer, this.max);
-  }
-}
-
-Tree.prototype.get = function() {
-  return this.tree;
-}
-
-Tree.prototype.push = function(v){
-  var len = this.tree.push(v);
-  if (this.max < len) {
-    this.layer += 1;
-    this.max += Math.pow(2, this.layer);
-  }
-  return len - 1;
-}
-
-Tree.prototype.switch = function(currIndex, layer, max){
-  var parentMax = max - Math.pow(2, layer);
-  var maxV = parentMax - Math.pow(2, layer-1);
-  var parentIndex = Math.ceil((currIndex+1-parentMax)/2) + maxV - 1;
-  var parentV = this.tree[parentIndex];
-  var currV = this.tree[currIndex];
-  if(layer > 0 && parentV < currV) {
-    this.tree[parentIndex] = currV;
-    this.tree[currIndex] = parentV;
-    this.switch(parentIndex, layer-1, parentMax);
-  }
-}
-
 function heapSort(list){
-  var sort = function(list) {
-    var tree = new Tree(list);
-    return tree.get();
+  var len = list.length;
+  var swap = function(a,b){
+    var temp = list[a];
+    list[a] = list[b];
+    list[b] = temp;
   }
-  var sortList = JSON.parse(JSON.stringify(list));
-  var result = [];
-
-  while(sortList.length) {
-   sortList = sort(sortList);
-   result.unshift(sortList.shift());
+  while(len){
+    for(var i=Math.floor(len/2)-1; i>=0; i--){
+      var curr = i;
+      var left = 2*i+1;
+      var right = 2*i+2;
+      if(left<len&&list[curr]<list[left]) curr = left;
+      if(right<len&&list[curr]<list[right]) curr = right;
+      if(curr!=i) swap(i, curr);
+    }
+    swap(0, len-1);
+    len -= 1;
   }
-
-  return result;
+  return list
 }
