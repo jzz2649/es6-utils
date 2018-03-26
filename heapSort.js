8a87164a7794e6d1bed9988b3cdd378bc87b6371
jzz2649 <1563382649@qq.com>
2018-03-27 00:51:28 +0800

    Update heapSort.js

--- heapSort.js
+++ heapSort.js
@@ -1,3 +1,4 @@
+
 function heapSort(list){
   var len = list.length;
   var swap = function(a,b){
@@ -5,17 +6,23 @@ function heapSort(list){
     list[a] = list[b];
     list[b] = temp;
   }
-  while(len){
-    for(var i=Math.floor(len/2)-1; i>=0; i--){
-      var curr = i;
-      var left = 2*i+1;
-      var right = 2*i+2;
-      if(left<len&&list[curr]<list[left]) curr = left;
-      if(right<len&&list[curr]<list[right]) curr = right;
-      if(curr!=i) swap(i, curr);
+  var adjust = function(i, j){
+    var curr = i;
+    var left = 2*i+1;
+    var right = 2*i+2;
+    if(left<j&&list[curr]<list[left]) curr = left;
+    if(right<j&&list[curr]<list[right]) curr = right;
+    if(curr!=i){
+      swap(i, curr);
+      adjust(curr, j);
     }
-    swap(0, len-1);
-    len -= 1;
+  }
+  for(var i=Math.floor(len/2)-1; i>=0; i--){
+    adjust(i, len);
+  }
+  while(len--){
+    swap(0, len);
+    adjust(0, len);
   }
   return list
 }
