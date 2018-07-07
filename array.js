bd061a92cfcaec11c51102bf89553a39bb5a9e9f
jzz7280 <1563382649@qq.com>
2018-07-08 00:10:24 +0800

    update

--- array.js
+++ array.js
@@ -1,1 +1,11 @@
 const distinct=s=>[...new Set(s)];
+
+const toTwoArr = (index, col) => {
+  const x = Math.floor(index/col);
+  const y = index%col;
+  return [x,y];
+}
+
+const toOneArr = (x, y, col) => {
+  return x*col + y;
+}
