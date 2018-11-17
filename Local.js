5deb06f2dbc648e74f62252dbbfd34238c0aec48
jzz2649 <1563382649@qq.com>
2018-11-17 12:04:13 +0800

    update

--- Local.js
+++ Local.js
@@ -1,30 +1,78 @@
 function Local(store){
-  let local = {};
+  let local = Object.create(null);
+  let length = 0;
+  const event = [];
+  const change = () => {
+    event.forEach(fn => {
+      fn.call(storage, storage.getAll());
+    })
+  }
   const storage = {
     set(k, v) {
+      if(typeof v !== 'string'){
+        v = String(v);
+      }
+      if(local[k] === v){
+        return;
+      }
+      if(local[k] === undefined){
+        ++length;
+      }
       local[k] = v;
       localStorage.setItem(k, v);
+      change();
     },
     get(k) {
-      return k ? local[k]: local;
+      if(local[k] === undefined){
+        return null;
+      }
+      return local[k];
     },
-    rm(k) {
+    getAll(){
+      return Object.assign({}, local);
+    },
+    remove(k) {
+      if(local[k] === undefined){
+        return;
+      }
+      --length;
       delete local[k];
       localStorage.removeItem(k);
+      change();
     },
-    cls() {
-      local = {};
+    clear() {
+      if(length === 0){
+        return;
+      }
+      length = 0;
+      local = Object.create(null);
       localStorage.clear();
+      change();
+    },
+    on(fn){
+      const index = event.indexOf(fn);
+      if(index > -1){
+        return;
+      }
+      event.push(fn);
+    },
+    off(fn){
+      const index = event.indexOf(fn);
+      if(index > -1){
+        event.splice(index, 1);
+      }
+    },
+    get length(){
+      return length;
     }
   }
   Object.keys(localStorage).forEach(k=>{
-      local[k] = localStorage.getItem(k);
+      storage.set(k, localStorage.getItem(k));
   })
-  if(store){
-    storage.cls();
-    for(const k in store){
+  if(typeof store === 'object' && store !== null){
+    Object.keys(store).forEach(k=>{
       storage.set(k, store[k]);
-    }
+    })
   }
   return storage;
 }
