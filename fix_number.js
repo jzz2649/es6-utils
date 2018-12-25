8369fe38444b11245e4021389b4922bdcde3027d
jzz2649 <1563382649@qq.com>
2018-12-25 17:46:04 +0800

    Update fix_number.js

--- fix_number.js
+++ fix_number.js
@@ -1,16 +1,33 @@
 function getFix(n){
   let fix = 1;
-  while(Math.floor(n)<n){
+  let f = Math.floor(n);
+  while(f<n){
+    if(f-n!==0){
+      const p = Number.EPSILON*fix;
+      const min = Math.abs(f-n);
+      const max = Math.abs(1-min);
+      if(min<p){
+        n = f;
+        break;
+      }
+      if(max<p){
+        n = f+1;
+        break;
+      }
+    }
     fix *= 10;
     n *= 10;
+    f = Math.floor(n);
   }
-  return fix;
+  return [n, fix];
 }
 function operate(a, b){
-  const fix = Math.max(getFix(a), getFix(b));
-  a *= fix;
-  b *= fix;
-  return [a, b, fix];
+  const [x, xfix] = getFix(a);
+  const [y, yfix] = getFix(b);
+  if(xfix>yfix){
+    return [x, y*(xfix/yfix), xfix];
+  }
+  return [x*(yfix/xfix), y, yfix];
 }
 function add(a, b){
   const [x, y, fix] = operate(a, b);
